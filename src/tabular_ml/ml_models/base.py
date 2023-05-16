48ce3fdd904abcc5119bd6fe376cac62d05f23b3
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-05-15 22:20:13 -0400

    Re-org

---
+++ src/tabular_ml/ml_models/base.py
@@ -0,0 +1,50 @@
+"""Base class for ML models"""
+import abc
+import numpy as np
+import pandas as pd
+from optuna import Trial
+from typing import (
+    Any,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+)
+
+
+class MLModel(abc.ABC):
+    """Signature for a ML model"""
+
+    @abc.abstractclassmethod
+    def train_model(
+        x_train: pd.DataFrame,
+        y_train: pd.Series,
+        model_params: Dict[str, Any],
+        weights_train: Optional[pd.Series] = None,
+        categorical_features: Optional[List[str]] = None,
+    ) -> object:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def make_predictions(
+        x_train: pd.DataFrame,
+        y_train: pd.Series,
+        x_test: pd.DataFrame,
+        model_params: Dict[str, Any],
+        weights_train: Optional[pd.Series] = None,
+        categorical_features: Optional[List[str]] = None,
+    ) -> Tuple[object, np.ndarray]:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def objective(
+        trial: Trial,
+        features: pd.DataFrame,
+        target: pd.Series,
+        kfolds: int,
+        metric_function: callable,
+        weights: Optional[pd.Series] = None,
+        categorical_features: Optional[List[str]] = None,
+        random_state: Optional[int] = None,
+    ) -> float:
+        raise NotImplementedError
