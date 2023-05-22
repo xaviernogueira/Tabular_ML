3e6c3d8956eb1ed6194fe2992b30eb9355688b45
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-05-22 18:53:03 -0400

    Update __init__.py

--- src/tabular_ml/__init__.py
+++ src/tabular_ml/__init__.py
@@ -3,7 +3,12 @@ __version__ = '0.0.1'
 from tabular_ml.factory import ModelFactory
 import tabular_ml.ml_models as ml_models
 from tabular_ml.functions import (
-    KFoldOutput,
     k_fold_cv,
     find_optimal_parameters,
 )
+
+__all__ = [
+    'ModelFactory',
+    'k_fold_cv',
+    'find_optimal_parameters',
+]
